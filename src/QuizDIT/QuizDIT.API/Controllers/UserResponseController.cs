02f66f11872e1b6dc7cf1b79c5d68f2c8fced84f
Shivendra Aggarwal <shivendra.aggarwal@outlook.com>
2019-04-28 06:51:19 +0530

    added new user response controller

---
+++ src/QuizDIT/QuizDIT.API/Controllers/UserResponseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuizDIT.Data.EFCore;
+using QuizDIT.Domain;
+
+namespace QuizDIT.API.Controllers
+{
+    [Route("v{version:apiVersion}/[controller]")]
+    public class UserResponseController : BaseController
+    {
+        private readonly QuizDITDbContext _context;
+        public UserResponseController(IServiceProvider serviceProvider, QuizDITDbContext context)
+            : base(serviceProvider)
+        {
+            _context = context;
+        }
+
+        [Route("user/{userid}/quiz/{quizid}/question/{questionid}/response/{responseid}")]
+        [HttpPost]
+        public IActionResult SubmitResponse(int userid,int quizid,int questionid,int responseid)
+        {
+            var userQuiz = _context.UserQuiz.FirstOrDefault(uq => uq.UserId == userid && uq.QuizId == quizid);
+
+            if (userQuiz != null)
+            {
+                var quizResponse = new UserQuizResponse();
+                quizResponse.IsCorrect = true;
+                quizResponse.UserQuizId = userQuiz.UserQuizId;
+                quizResponse.IsDeleted = false;
+                quizResponse.QuestionId = questionid;
+
+                _context.Add(quizResponse);
+
+                _context.SaveChanges();
+
+            }
+
+
+            return Ok();
+        }
+    }
+}
