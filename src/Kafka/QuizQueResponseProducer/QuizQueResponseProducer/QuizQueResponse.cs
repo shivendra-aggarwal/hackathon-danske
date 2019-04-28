1610a1c4fe65f5504258847fce16c3ade50eeda7
PrashantNavale <tn.prashant@gmail.com>
2019-04-28 08:37:55 +0530

    added kafka message broker framework support

---
+++ src/Kafka/QuizQueResponseProducer/QuizQueResponseProducer/QuizQueResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizQueResponseProducer
+{
+    public class QuizQueResponse
+    {
+        public int UserId { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionId { get; set; }
+        public int ResponseId { get; set; }
+    }
+}
